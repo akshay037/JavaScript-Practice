8efe85fdb40fa18766b52972e05e7ccd7e140810
akshay037 <akshayteltumbade69@gmail.com>
2023-03-23 19:27:12 +0530

    Variable, Constant and Let declaration

---
+++ Variable.js
@@ -0,0 +1,21 @@
+// Variables - To assign value
+var firstName = 'Pentacost';
+
+var passion;
+passion = 'Gaming';
+
+var rating = 7.5
+
+console.log(firstName, passion, rating)
+
+// Const - Cannot changes the value or re assign
+const Name = 'Ironman'
+
+console.log(Name, )
+
+// Let - To change perticular value as per need when we wanted 
+{
+    let passion = 'Photgraphy';
+    console.log(passion);
+}
+console.log(passion);
