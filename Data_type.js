5aa72921794642a41000e4d7143dc133c63757ed
akshay037 <akshayteltumbade69@gmail.com>
2023-03-24 17:02:14 +0530

    Diffrent data types example

---
+++ Data_type.js
@@ -0,0 +1,41 @@
+//Primitive data types
+// String 
+let firstName = 'Buntu';
+console.log('This is my name '+ firstName);
+console.log('Data type is ' + (typeof firstName));
+
+// Number
+let marks = 68;
+console.log('I got ' + marks + ' marks');
+console.log('Data type is ' + (typeof marks));
+
+// Boolean
+let isDriver = true;
+console.log('Data type is ' + (typeof isDriver));
+
+// Null
+let nullVar = null;
+console.log('Data type is ' + (typeof nullVar));
+
+// Undefine
+let undef = undefined;
+console.log('Data type is ' + (typeof undef));
+
+// Reference data types
+// Array
+let myarr = [1,2,3,4, false, 'Buntu'];
+console.log('Data type is ' + (typeof myarr));
+
+// Object literals
+let percentage = {
+    Harry: 89,
+    Hermiony: 95,
+    Ronald: 60
+}
+console.log('Data type is ' + (typeof percentage));
+
+// Function
+function findName(){
+
+}
+console.log('Data type is ' + (typeof findName));
