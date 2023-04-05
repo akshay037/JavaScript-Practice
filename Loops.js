e554e733475e61ebfc059a64115ba2f8c916c693
akshay037 <akshayteltumbade69@gmail.com>
2023-04-05 14:55:39 +0530

    For, while and Do-while loops

---
+++ Loops.js
@@ -0,0 +1,20 @@
+// for loop 
+
+for(let i=0;i<=20;i++){
+    console.log(i);
+}
+
+// while loop
+
+let j=0;
+while(j<10){
+    console.log(j++); 
+}
+
+// dowhile loop
+
+let k = 0;
+do {
+    console.log(k++);
+}
+while (k<10);
