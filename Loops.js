bbbf226c053ed2428631fcf941495839d4c6e79f
akshay037 <akshayteltumbade69@gmail.com>
2023-04-06 13:31:04 +0530

    Loops for Array and Object

--- Loops.js
+++ Loops.js
@@ -17,4 +17,36 @@ let k = 0;
 do {
     console.log(k++);
 }
-while (k<10);
+while (k<=10);
+
+console.log('Done')
+
+// Break
+
+let X = 0;
+do {
+    console.log(X + 1);
+    if(X===5){
+        break;
+    }
+    X += 1;
+}
+while (X<10);
+
+console.log('Done')
+
+let arr = [41,48,34,65,67]
+arr.forEach(function(element, index){
+    console.log(element, index)
+});
+console.log(arr)
+
+let obj = {
+    names:"Tony",
+    age:"28",
+    sex:"Male",
+    Power:"Ironman suit, Friday OS",
+}
+for(let key in obj){
+    console.log(`The ${key} of object is ${obj[key]}`)
+}
